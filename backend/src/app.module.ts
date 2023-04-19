e4e81fd6632a9b9349e026095452557fddffef4f
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-19 02:14:53 -0300

    Setup nestjs to use env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [],
   providers: [],
 })
