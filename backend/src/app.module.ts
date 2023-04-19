220ea35e472320450b0ceb841a3992e367790ca9
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-19 02:28:03 -0300

    Connect to mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({isGlobal: true})],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI || "")
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
