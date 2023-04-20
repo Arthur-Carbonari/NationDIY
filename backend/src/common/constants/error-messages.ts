c8ee9f87711abbcbed979a5374dab54ce3e3407f
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-20 00:21:13 -0300

    Add new error messages

--- backend/src/common/constants/error-messages.ts
+++ backend/src/common/constants/error-messages.ts
@@ -4,5 +4,9 @@ export default class ErrorMessage {
   static readonly PASSWORDS_DO_NOT_MATCH = 'The passwords you entered do not match.';
   static readonly INVALID_CREDENTIALS = 'Invalid email or password.';
   static readonly INTERNAL_SERVER_ERROR = 'Something went wrong on our end. Please try again later.';
+  static readonly INVALID_EMAIL: "Please enter a valid email address.";
+  static readonly USERNAME_TAKEN: "This username is already taken. Please choose a different username.";
+  static readonly WEAK_PASSWORD: "Your password should be at least 8 characters long and include a combination of uppercase and lowercase letters, numbers, and symbols."
+  static readonly INVALID_URL: "Invalid URL"
   // Add more error messages as needed
 }
