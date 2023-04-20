2b8297cf1681e0fff57802ab8cb9441a2bd3cffd
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-20 04:08:14 -0300

    Import ConfigModule into auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
@@ -7,6 +8,7 @@ import { AuthService } from './auth.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     PassportModule.register({defaultStrategy: "jwt"}),
     JwtModule.register({secret: process.env.JWT_SECRET, signOptions: {expiresIn: process.env.JWT_EXPIRES}}),
     UsersModule
