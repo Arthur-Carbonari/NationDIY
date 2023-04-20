67f7116e58f1a0c1dce7ed69860fef33fe0bb3b5
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-20 03:52:09 -0300

    Create error message for username minimal length

--- backend/src/common/constants/error-messages.ts
+++ backend/src/common/constants/error-messages.ts
@@ -6,6 +6,7 @@ export default class ErrorMessage {
   static readonly INTERNAL_SERVER_ERROR = 'Something went wrong on our end. Please try again later.';
   static readonly INVALID_EMAIL: "Please enter a valid email address.";
   static readonly USERNAME_TAKEN: "This username is already taken. Please choose a different username.";
+  static readonly USERNAME_MIN_LENGTH = "Username needs to be at least 5 characters long."
   static readonly WEAK_PASSWORD: "Your password should be at least 8 characters long and include a combination of uppercase and lowercase letters, numbers, and symbols."
   static readonly INVALID_URL: "Invalid URL"
   // Add more error messages as needed
