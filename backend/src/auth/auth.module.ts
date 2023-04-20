d9bc32957f525f6c68fb0a3035d3d5d2f0d57875
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-20 01:22:08 -0300

    Import necessary nestjs modules in Auth Module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule.register({defaultStrategy: "jwt"}),
+    JwtModule.register({secret: process.env.JWT_SECRET, signOptions: {expiresIn: process.env.JWT_EXPIRES}}),
+    UsersModule
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
