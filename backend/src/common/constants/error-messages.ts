c74f5d38eea3653d225d1a7614a025b4cf363812
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-19 23:57:12 -0300

    Create static class ErrorMessage to contain all the error messages used thoughout the website

---
+++ backend/src/common/constants/error-messages.ts
@@ -0,0 +1,8 @@
+export default class ErrorMessage {
+  static readonly REQUIRED_FIELD_EMPTY = 'This field is required.';
+  static readonly EMAIL_ALREADY_REGISTERED = 'The email address you entered is already registered.';
+  static readonly PASSWORDS_DO_NOT_MATCH = 'The passwords you entered do not match.';
+  static readonly INVALID_CREDENTIALS = 'Invalid email or password.';
+  static readonly INTERNAL_SERVER_ERROR = 'Something went wrong on our end. Please try again later.';
+  // Add more error messages as needed
+}
