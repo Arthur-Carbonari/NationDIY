1bccb4372c909ea023c965a7ceb859c58710802e
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-19 22:12:38 -0300

    Create users module with nest cli

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URI || "")
+    MongooseModule.forRoot(process.env.MONGO_URI || ""),
+    UsersModule
   ],
   controllers: [],
   providers: [],
