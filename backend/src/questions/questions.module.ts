906076d281cc9c821aa308872c2c99b7b581dff2
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-24 21:55:41 -0300

    Import Auth Module into Question Module

--- backend/src/questions/questions.module.ts
+++ backend/src/questions/questions.module.ts
@@ -4,9 +4,11 @@ import { QuestionsController } from './questions.controller';
 import { Question, QuestionSchema } from './schema/question.schema';
 import { Answer, AnswerSchema } from './schema/answer.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: Question.name, schema: QuestionSchema }]),
     MongooseModule.forFeature([{ name: Answer.name, schema: AnswerSchema }]),
   ],
