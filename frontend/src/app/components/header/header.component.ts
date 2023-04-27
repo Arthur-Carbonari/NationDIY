ef6aa7b6ca296d4863dd5519070f34b7ee384cc7
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-27 04:33:44 -0300

    Connect login authentication service to login modal

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  title: string = 'NationDIY'
+export class HeaderComponent implements OnInit {
 
+  title: string = 'NationDIY'
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.authService.login('arthur@gmail.com', 'aW12@340').subscribe(res => { console.log('sucess', res); })
+  }
 }
