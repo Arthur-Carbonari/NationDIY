1b97750442b5369e1dd91049d9bdfec530cf4c1c
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-28 01:05:27 -0300

    Implement authentication service signup method

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -6,10 +6,17 @@ import { map } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthenticationService {
-
+  
   constructor(private httpClient: HttpClient) { }
-
-  register(){}
+  
+  signup(email: string, username: string, password: string) {
+    return this.httpClient.post<any>('api/auth/signup', { email, username, password }).pipe(
+      map((response) => {
+        localStorage.setItem('auth-token', response.token)
+        return response
+      })
+    )
+  }
 
   login(emailOrUsername: string, password: string) {
     return this.httpClient.post<any>('api/auth/login', { emailOrUsername, password }).pipe(
