4eca66b65ca8ea394598d936788c7eb5745be010
Arthur Martins <arthurcarbonari99@gmail.com>
2023-04-28 00:38:40 -0300

    Create move login form logic into login modal component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule } from '@angular/material/button';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +22,21 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+    LoginModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/login-modal/login-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Component({
+  selector: 'app-login-modal',
+  templateUrl: './login-modal.component.html',
+  styleUrls: ['./login-modal.component.scss']
+})
+export class LoginModalComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private authService: AuthenticationService, private router: Router) {
+
+    this.loginForm = new FormGroup({
+
+      // Email or Username field
+      emailOrUsername: new FormControl(null, [Validators.required]),
+
+      // Password field
+      password: new FormControl(null, [Validators.required])
+    })
+  }
+
+  onSubmit() {
+    
+    if (this.loginForm.invalid) {return};
+
+    const { emailOrUsername, password } = this.loginForm.value;
+
+    this.authService.login(emailOrUsername, password)
+      .subscribe({
+        next: (_response) => { this.router.navigateByUrl(this.router.url); },
+        error: (error) => { console.log(error); }
+      });
+
+  }
+
+}
