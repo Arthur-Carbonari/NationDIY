e6f26754d7f4733996433add9d03fcfbd2cea097
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-04 01:18:37 -0300

    Add observable isLoggedIn to AuthenticationService

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -1,34 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private httpClient: HttpClient) { }
+  private _isLoggedIn$ = new BehaviorSubject<boolean>(false)
+  public isLoggedIn$ = this._isLoggedIn$.asObservable()
+
+  constructor(private httpClient: HttpClient) {
+    const token = localStorage.getItem("auth-token")
+    this._isLoggedIn$.next(!!token)
+  }
 
   signup(email: string, username: string, password: string) {
     return this.httpClient.post<any>('api/auth/signup', { email, username, password }).pipe(
-      map((response) => {
+      tap((response) => {
+        this._isLoggedIn$.next(true)
         localStorage.setItem('auth-token', response.token)
-        return response
       })
     )
   }
 
   login(emailOrUsername: string, password: string) {
     return this.httpClient.post<any>('api/auth/login', { emailOrUsername, password }).pipe(
-      map((response) => {
+      tap((response) => {
+        this._isLoggedIn$.next(true)
         localStorage.setItem('auth-token', response.token)
-        return response
       })
     )
 
   }
 
-  checkAvailability(emailOrUsername: string){
+  checkAvailability(emailOrUsername: string) {
     return this.httpClient.post<any>('api/auth/check-availability', { emailOrUsername })
   }
 }
