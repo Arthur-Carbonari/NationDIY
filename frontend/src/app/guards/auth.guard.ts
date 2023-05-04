2559a61dfa1b84be98a4d79c3c39e37ea1f03637
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-04 03:43:33 -0300

    Create auth canActivate guard to to check if user is logged in

---
+++ frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  
+  constructor(private authService: AuthenticationService, private router: Router){}
+  
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isLoggedIn$.pipe(
+      tap( isLoggedIn => {
+        if(!isLoggedIn) this.router.navigate([''])
+      })
+    );
+  }
+  
+}
