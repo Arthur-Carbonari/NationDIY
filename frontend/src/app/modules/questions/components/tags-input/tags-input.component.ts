5637f4279a8424b5796296d3a0c39bfa917b97f7
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-08 16:24:32 -0300

    Create tags-input component

---
+++ frontend/src/app/modules/questions/components/tags-input/tags-input.component.ts
@@ -0,0 +1,126 @@
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, ElementRef, forwardRef, HostListener, ViewChild} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-tags-input',
+  templateUrl: './tags-input.component.html',
+  styleUrls: ['./tags-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TagsInputComponent),
+      multi: true
+    }
+  ]
+})
+/** Component for managing a list of tags input by the user, it implements the ControlValueAccessor so it will work with reactive forms. */
+export class TagsInputComponent implements ControlValueAccessor {
+  
+  /** Keys codes that can be used to separate tags. */
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+
+  /** Form control for managing the input value of the tag. */
+  tagCtrl = new FormControl('');
+
+  /** Observable that emits filtered tag values based on user input. */
+  filteredTags: Observable<string[]>;
+
+  /** Set of tags added by the user. */
+  tags = new Set<string>();
+
+  /** Array of all available tags. */
+  allTags: string[] = ["tools", "materials", "techniques", "repairs", "design"];
+
+  /** Reference to the input element for adding new tags. */
+  @ViewChild('tagInput')tagInput!: ElementRef<HTMLInputElement>;
+
+  /**
+   * Creates a new instance of the TagsInputComponent class.
+   * @constructor
+   */
+  constructor() {
+    this.filteredTags = this.tagCtrl.valueChanges.pipe(
+      startWith(null),
+      map((tag: string | null) => (tag ? this._filter(tag) : this.allTags.slice())),
+    );
+  }
+
+  /**
+   * Adds a new tag to the list of tags.
+   * @param event The MatChipInputEvent object for adding a new tag.
+   */
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our tag
+    if (value) {
+      this.tags.add(value);
+      this.updateControlValue()
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.tagCtrl.setValue(null);
+
+  }
+
+  /**
+   * Removes a tag from the list of tags.
+   * @param tag The tag to remove.
+   */
+  remove(tag: string): void {
+    this.tags.delete(tag)
+    this.updateControlValue()
+  }
+
+  /**
+   * Adds a selected tag to the list of tags.
+   * @param event The MatAutocompleteSelectedEvent object for selecting a tag.
+   */
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.tags.add(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagCtrl.setValue(null);
+    this.updateControlValue()
+  }
+
+  /**
+   * Filters available tags based on user input.
+   * @param value The value to use for filtering tags.
+   * @returns An array of filtered tag values.
+   */
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(tag => tag.toLowerCase().includes(filterValue));
+  }
+
+  /** This methods updates the value of this component control value to work with the angular form API */
+  updateControlValue(){
+    this.onChange(this.tags)
+  }
+
+  // ControlValueAcessor interface methods and properties, methods need to be implemented to comply with the interface
+  private onChange: (value: Set<string>) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  writeValue(tags: string[]): void {
+    if (tags) {
+      this.tags = new Set(tags);
+    }
+  }
+  registerOnChange(fn: (value: Set<string>) => void): void { this.onChange = fn;}
+  registerOnTouched(fn: () => void): void { this.onTouched = fn; }
+  setDisabledState?(isDisabled: boolean): void {}
+
+  // When this component triggers the focus out event it is marked as touched
+  @HostListener('focusout')
+  onFocusOut(){ this.onTouched(); }
+}
+
--- frontend/src/app/modules/questions/questions.module.ts
+++ frontend/src/app/modules/questions/questions.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// Angular material imports
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';;
+import { MatIconModule } from '@angular/material/icon';
+
 import { QuestionsRoutingModule } from './questions-routing.module';
+import { QuestionComponent } from './pages/question/question.component';
+import { AskQuestionComponent } from './pages/ask-question/ask-question.component';
+import { TagsInputComponent } from './components/tags-input/tags-input.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    QuestionComponent,
+    AskQuestionComponent,
+    TagsInputComponent
+  ],
   imports: [
     CommonModule,
-    QuestionsRoutingModule
+    QuestionsRoutingModule,
+
+    //tags input imports
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatChipsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatIconModule,
   ]
 })
 export class QuestionsModule { }
