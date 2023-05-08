7943b866e8a44258527955768dd72f27a0d33287
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-08 16:47:25 -0300

    Refactor module declarations and imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,33 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { HomeComponent } from './modules/home/pages/home/home.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule} from '@angular/material/input'
-import {MatButtonModule } from '@angular/material/button';
-import { AskQuestionComponent } from './modules/questions/pages/ask-question/ask-question.component';
-import { QuestionComponent } from './modules/questions/pages/question/question.component';
 
 import { CoreModule } from './core/core.module';
 import { ModalFormsModule } from './modules/modal-forms/modal-forms.module';
-import { LoginModalComponent } from './modules/modal-forms/components/login-modal/login-modal.component';
-import { SignupModalComponent } from './modules/modal-forms/components/signup-modal/signup-modal.component';
 import { HomeModule } from './modules/home/home.module';
+import { QuestionsModule } from './modules/questions/questions.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    LoginModalComponent,
-    SignupModalComponent,
-    AskQuestionComponent,
-    QuestionComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,19 +29,13 @@ import { HomeModule } from './modules/home/home.module';
     MatToolbarModule,
     HttpClientModule,
 
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-
     // core & shared module
     CoreModule,
 
-    
     // my modules
     ModalFormsModule,
-    HomeModule
+    HomeModule,
+    QuestionsModule
 
   ],
   providers: [],
--- frontend/src/app/modules/modal-forms/modal-forms.module.ts
+++ frontend/src/app/modules/modal-forms/modal-forms.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SignupModalComponent } from './components/signup-modal/signup-modal.component';
+import { LoginModalComponent } from './components/login-modal/login-modal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [
+    SignupModalComponent,
+    LoginModalComponent
+  ],
+  imports: [
+    CommonModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  
+  ],
+  exports: [
+    SignupModalComponent,
+    LoginModalComponent
+  ]
 })
 export class ModalFormsModule { }
