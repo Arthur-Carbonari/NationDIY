3f4b09ede1c49a5ecea9752a970073ee851807dc
Vinicius Pepe Bellomo <vinicius.pepebellomo@student.griffith.ie>
2023-05-10 21:08:40 +0100

    fix path for all question on home page

--- frontend/src/app/shared/question.interface.ts
+++ frontend/src/app/shared/question.interface.ts
@@ -5,7 +5,7 @@ export interface Question {
     tags: string;
     body: string;
     author: string;
-    votes: {value: number, user: string}[];  // check if this one must be Vote [] 
+    votes: {value: number, user: string}[]; 
     comments: Comment[];
     answers: string[];
     createdAt: string;
