325e18886442a9a49a41159d99c0977d7b21f3f4
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-09 23:03:01 -0300

    Create questions service

---
+++ frontend/src/app/modules/questions/services/questions.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Question } from 'src/app/shared/question.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionsService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getQuestions(): Observable<Question[]>{
+    return this.httpClient.get<any>('api/questions')
+  }
+}
