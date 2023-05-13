85304617ed60c2df7fb3f32061b21701a7f65ca6
Arthur Martins <arthurcarbonari99@gmail.com>
2023-05-12 23:35:04 -0300

    Fix bug on text editor

--- frontend/src/app/shared/components/text-editor/text-editor.component.ts
+++ frontend/src/app/shared/components/text-editor/text-editor.component.ts
@@ -25,8 +25,6 @@ export class TextEditorComponent implements ControlValueAccessor, OnInit, OnDest
     ['ordered_list', 'bullet_list'],
     [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
     ['link', 'image'],
-    ['text_color', 'background_color'],
-    ['align_left', 'align_center', 'align_right', 'align_justify'],
   ];
 
   onChange = (value: string) => {};
